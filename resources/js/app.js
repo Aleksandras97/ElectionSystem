b0a150318f8866734317d6d08880b193efc9db4b
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-10 16:13:52 +0300

    Updated from blade views to vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,19 @@
 require('./bootstrap');
 
 
+
+import { createApp } from 'vue';
+import Master from "./layouts/Master";
+import router from './routes';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+createApp({
+    components: {
+        Master
+    }
+}).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
