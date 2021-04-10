f2eeca724fbecd5af91492a80c90b5cd14d861a7
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-10 22:34:23 +0300

    Deleted navbar list

--- resources/js/vue/Header.vue
+++ resources/js/vue/Header.vue
@@ -1,20 +1,16 @@
 <template>
-    <header class="py-5 bg-gray-700 text-white text-center">
-        <nav class="flex justify-between py-5 px-1.5 text-6xl">
-            <div class="flex ">
-                <div class="font-bold border-2 border-white p-2">Election System</div>
-                <div class="flex font-bold">
-                    <HeaderLink to="/" exact>Home</HeaderLink>
-                    <HeaderLink to="/elections" exact>Elections</HeaderLink>
+    <header class="bg-white shadow-sm w-full fixed z-10  top-0">
+        <div class="max-auto px-8 py-2 bg-white">
+            <div class="flex justify-between">
+                <div class="logo flex items-center space-x-4 mr-10">
+                    <font-awesome-icon class="h-8 text-blue-400" icon="person-booth"/>
+                    <h1 class="text-gray-600 text-2xl">Election System</h1>
+                </div>
+                <div class="menu flex justify-end items-center flex-1 space-x-4">
+                    <h1 class="text-gray-600 text-2xl">Username</h1>
                 </div>
             </div>
-
-            <div class="navigation__user">
-                Username
-            </div>
-        </nav>
-
-
+        </div>
     </header>
 
 </template>
