057e2c078279ac553d7a9c9ae0f130603871c18b
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-16 23:36:57 +0300

    Added Login and Logout API functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,25 +4,54 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use GuzzleHttp\Exception;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function store(Request $request): JsonResponse
+    public function login(Request $request)
     {
-
-        $credentials = $request->validate([
-            'email' => 'email|required',
-            'password' => 'required',
+        $http = new \GuzzleHttp\Client;
+        $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
         ]);
 
-        if (!auth::attempt($credentials))
-        {
-            return response()->json(['message' => 'Invalid credentials']);
+
+        try {
+            $response = $http->request('POST', config('services.passport.login_endpoint'), [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => config('services.passport.client_id'),
+                    'client_secret' => config('services.passport.client_secret'),
+                    'username' => $request->username,
+                    'password' => $request->password,
+                ]
+            ]);
+
+            return $response->getBody();
+        } catch (BadResponseException $e) {
+//            dd($e);
+            if ($e->getCode() == 400){
+                return response()->json('Your credentials are incorrect. Please try again.', $e->getCode());
+            } else if ($e->getCode() == 401) {
+                return response()->json('Your credentials are incorrect. Please try again.', $e->getCode());
+            }
+//
+            return response()->json('Your credentials are incorrect. Please try again.', $e->getCode());
         }
-        $token = auth()->user()->createToken('authToken')->accessToken;
-        return response()->json(['token' => $token]);
+    }
+
+    public function logout(): JsonResponse
+    {
+        auth()->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+
+        return response()->json('Logged out successfully', 200);
     }
 }
