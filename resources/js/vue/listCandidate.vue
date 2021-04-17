cd02381fe890c8ccfbc88570c451daaa14db93af
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-17 23:06:26 +0300

    Updated candidate list view

--- resources/js/vue/listCandidate.vue
+++ resources/js/vue/listCandidate.vue
@@ -1,27 +1,45 @@
 <template>
-    <div class="item">
-        <span> {{ candidate.firstname }} </span>
-        <br>
-        <span> {{ candidate.lastname }} </span><br>
-        <span> {{ candidate.birthdate }} </span><br>
-        <span> {{ candidate.city }} </span><br>
-        <span> {{ candidate.district }} </span><br>
-        <span> {{ candidate.street_address }} </span><br>
-        <span> {{ candidate.gender }} </span><br>
+    <tr>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <p class="text-gray-900 whitespace-no-wrap text-center">{{ index + 1  }} and {{ candidate.id }}</p>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.firstname }}</p>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.lastname }}</p>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.birthdate }}</p>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <label class="flex justify-center ml-6">
+                <input v-model="state.candidate_id" v-on:click="$emit('selected-candidate', candidate.id)" type="radio" class="form-radio w-8 h-8" name="accountType">
+            </label>
+        </td>
+    </tr>
+
 
 <!--        <button @click="deleteElection" class="trashcan">-->
 <!--            <font-awesome-icon icon="trash" />-->
 <!--        </button>-->
-    </div>
 </template>
 
 <script>
+import {reactive} from "vue";
+
 export default {
     props: {
         candidate: Object,
+        index: Number,
     },
     setup(){
-
+        const state = reactive({
+            candidate_id: null
+        });
+        return {
+            state
+        }
     }
 }
 </script>
