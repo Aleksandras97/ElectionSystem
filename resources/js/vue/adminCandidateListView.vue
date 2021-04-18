72c8da05631f7e995686b68fb9fd63ed6ac247f4
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-19 01:24:25 +0300

    Added paginated candidate list view

---
+++ resources/js/vue/adminCandidateListView.vue
@@ -0,0 +1,27 @@
+<template>
+    <tr v-for="(candidate, index) in candidates" :key="index">
+
+        <AdminListCandidate
+            :candidate="candidate"
+            v-on:candidates-update="$emit('reload-candidates')"
+
+        />
+    </tr>
+</template>
+
+<script>
+import AdminListCandidate from "./adminListCandidate";
+export default {
+    components: {AdminListCandidate},
+    emits: {
+        'reload-candidates': null,
+    },
+    props: {
+        candidates: Array,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
