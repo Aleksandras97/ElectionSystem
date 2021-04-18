c1c43a997a37e289dac579883e1c6d71052e04fe
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-19 01:24:38 +0300

    Added paginated elections list view

---
+++ resources/js/vue/adminElectionListView.vue
@@ -0,0 +1,28 @@
+<template>
+    <tr v-for="(election, index) in elections" :key="index">
+
+<!--        <router-link :to="{ name: 'AdminCandidates', params: { electionId: election.id }}" exact>-->
+        <AdminListElection
+            :election="election"
+            v-on:elections-update="$emit('reload-elections')"
+
+        />
+<!--        </router-link>-->
+    </tr>
+</template>
+
+<script>
+import AdminListElection from "./adminListElection";
+export default {
+    components: {AdminListElection},
+    emits: {
+        'reload-elections': null,
+    },
+    props: {
+        elections: Array,
+    },
+    setup(){
+
+    }
+}
+</script>
