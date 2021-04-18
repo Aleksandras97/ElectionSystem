f96fc5c0563ad1566682a61cc62af1d503442fa1
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-19 01:26:16 +0300

    Updated election list view

--- resources/js/vue/electionListView.vue
+++ resources/js/vue/electionListView.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div v-for="(election, index) in elections?.slice().reverse()" :key="index">
+    <div v-for="(election, index) in elections" :key="index">
 
         <router-link :to="{ name: 'Candidates', params: { electionId: election.id }}" exact>
             <ListElection
@@ -13,15 +13,11 @@
 </template>
 
 <script>
-import listItem from "./listElection";
 import {useRouter} from "vue-router";
 import ListElection from "./listElection";
-import HeaderLink from "./HeaderLink";
 export default {
     components: {
-        ListElection,
-        listItem,
-        HeaderLink
+        ListElection
     },
     emits: {
         'reload-elections': null
