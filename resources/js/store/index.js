319ca2d6bd7127aca806bccb4abc942a20614724
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-21 01:20:59 +0300

    Add and Remove notification to vuex

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -4,16 +4,34 @@ import { AuthModule } from './auth'
 
 
 const state = {
-
+    notifications: [],
 }
 const getters = {
 
 }
 const mutations = {
+    PUSH_NOTIFICATION(state, notification) {
+        state.notifications.push(notification)
 
+    },
+    REMOVE_NOTIFICATION(state, notificationToRemove) {
+        state.notifications = state.notifications.filter(notification => {
+            return notification.id !== notificationToRemove.id;
+        })
+    }
 }
 
 const actions = {
+    async addNotification ({ commit, dispatch }, notification) {
+        commit('PUSH_NOTIFICATION', notification);
+        await new Promise(
+            (resolve, reject) => setTimeout(() => resolve(), 4000));
+        dispatch('removeNotification', notification);
+    },
+    removeNotification ({ commit }, notification) {
+        commit('REMOVE_NOTIFICATION', notification);
+    },
+
 
 
 
