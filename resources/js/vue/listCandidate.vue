7eb2652b14dde3028117fe9b5975ab5640ec4505
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-21 23:56:32 +0300

    Added bigger text

--- resources/js/vue/listCandidate.vue
+++ resources/js/vue/listCandidate.vue
@@ -1,18 +1,18 @@
 <template>
     <tr>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
             <p class="text-gray-900 whitespace-no-wrap text-center">{{ index + 1  }} and {{ candidate.id }}</p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
             <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.firstname }}</p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
             <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.lastname }}</p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
             <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.birthdate }}</p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
             <label class="flex justify-center ml-6">
                 <input v-model="state.candidate_id" v-on:click="$emit('selected-candidate', candidate.id)" type="radio" class="form-radio w-8 h-8" name="accountType">
             </label>
