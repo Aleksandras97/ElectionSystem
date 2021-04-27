f8c99f45cdbb9ec27586305accfeaa3498c8d589
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-28 00:30:23 +0300

    Updated Election page view

--- resources/js/views/Elections.vue
+++ resources/js/views/Elections.vue
@@ -1,27 +1,35 @@
 <template>
-    <div class="flex justify-start mb-1 sm:mb-0">
-        <div class="relative ml-2">
-                        <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                            <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
-                                <path
-                                    d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
-                                </path>
-                            </svg>
-                        </span>
-            <input v-model="state.search" placeholder="Search"
-                   class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+
+
+    <div class="m-2 grid grid-cols-6 gap-4">
+        <div class="col-start-2 col-end-5">
+            <h1 class="flex justify-center text-6xl font-normal leading-normal mt-0 mb-2 text-gray-800">
+                Elections
+            </h1>
+
+            <div class="py-2 sm:mb-0">
+                <div class="relative">
+                                <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                                    <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
+                                        <path
+                                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                                        </path>
+                                    </svg>
+                                </span>
+                    <input v-model="state.search" placeholder="Search"
+                        class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
+
+            </div>
+
+            <election-list-view
+                :elections="state.elections"
+                v-on:reload-elections="getElections()"
+            />
+
         </div>
-
     </div>
 
-    <div
-        class="m-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2x1:grid-cols-6 gap-4">
-
-        <election-list-view
-            :elections="state.elections"
-            v-on:reload-elections="getElections()"
-        />
-    </div>
     <div
         class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
                         <span class="text-xs xs:text-sm text-gray-900">
