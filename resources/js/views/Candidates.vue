f41221bb80108c757b49fe1788759750101f7ee9
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-28 00:31:11 +0300

    Updated candidate page view

--- resources/js/views/Candidates.vue
+++ resources/js/views/Candidates.vue
@@ -1,70 +1,93 @@
 <template>
-    {{ state.election?.election_date }}
-    <pie-chart v-if="!isSameDay(state.election?.election_date) && isPastDay(state.election?.election_date)"
-               :data="state.data"></pie-chart>
 
-    <candidateListView
-        :candidates="state.candidates"
-        v-on:candidate="state.candidate_id = $event"
-    />
-    <div
-        class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
-        <span class="text-xs xs:text-sm text-gray-900">
-            Showing {{ pagination.current_page }} of {{ pagination.last_page }} Entries
-        </span>
-        <div class="inline-flex mt-2 xs:mt-0">
-            <button
-                class="bg-yellow-500 hover:bg-yellow-400 border-yellow-700 hover:border-yellow-500 text-white font-bold py-1 px-4 ml-3 border-b-4 rounded"
-                :disabled='!pagination.prev_page_url'
-                :class="{'opacity-50': !pagination.prev_page_url }"
-                @click="getCandidates(pagination.prev_page_url)"
-            >
-                Prev
-            </button>
-            <button
-                class="bg-yellow-500 hover:bg-yellow-400 border-yellow-700 hover:border-yellow-500 text-white font-bold py-1 px-4 ml-3 border-b-4 rounded"
-                :disabled='!pagination.next_page_url'
-                :class="{'opacity-50': !pagination.next_page_url }"
-                @click="getCandidates(pagination.next_page_url)"
-            >
-                Next
-            </button>
+
+
+    <div class="grid grid-cols-1 gap-4 place-items-center mb-10">
+        <div>
+            <h1 class="flex justify-center text-6xl font-bold leading-normal mt-0 mb-2 text-gray-800">
+                {{ state.election?.election_name }}
+            </h1>
+        </div>
+        <div>
+            <h1 class="flex justify-center text-4xl font-normal leading-normal mt-0 mb-2 text-gray-500">
+                {{ state.election?.election_date }}
+            </h1>
+        </div>
+        <div>
+            <template v-if="!state.Voted">
+                <template v-if="isSameDay(state.election?.election_date)">
+                    <button @click="isModalOpen = true"
+                            class="bg-purple-500 hover:bg-purple-400 border-purple-700 hover:border-purple-500 text-white font-bold py-1 px-4 ml-3 border-b-4 rounded">
+                        Submit Vote
+                        <font-awesome-icon
+                            icon="check"
+                        />
+                    </button>
+                </template>
+                <template v-if="!isSameDay(state.election?.election_date)">
+                    <template v-if="!isPastDay(state.election?.election_date)">
+                        <span
+                            class="bg-yellow-200 text-yellow-600 py-2 px-5 rounded-full text-2xl">Coming soon</span>
+                    </template>
+                    <template v-if="isPastDay(state.election?.election_date)">
+                        <span class="bg-red-200 text-red-600 py-2 px-5 rounded-full text-2xl">Election is over</span>
+
+                    </template>
+                </template>
+
+            </template>
+            <template v-else-if="state.Voted">
+                <span class="bg-green-200 text-green-600 py-2 px-5 rounded-full text-2xl">Voted</span>
+                <template v-if="isPastDay(state.election?.election_date)">
+                    <span class="bg-red-200 text-red-600 py-2 px-5 rounded-full text-2xl">Election is over</span>
+                </template>
+            </template>
         </div>
     </div>
 
-    <div class="flex justify-center pb-4">
-
-        <template v-if="!state.Voted">
-            <template v-if="isSameDay(state.election?.election_date)">
-                <button @click="isModalOpen = true"
-                        class="bg-purple-500 hover:bg-purple-400 border-purple-700 hover:border-purple-500 text-white font-bold py-1 px-4 ml-3 border-b-4 rounded">
-                    Submit Vote
-                    <font-awesome-icon
-                        icon="check"
-                    />
-                </button>
-            </template>
-            <template v-if="!isSameDay(state.election?.election_date)">
-                <template v-if="!isPastDay(state.election?.election_date)">
-                    <span
-                        class="bg-yellow-200 text-yellow-600 py-2 px-5 rounded-full text-2xl">Election is coming soon</span>
-                </template>
-                <template v-if="isPastDay(state.election?.election_date)">
-                    <span class="bg-red-200 text-red-600 py-2 px-5 rounded-full text-2xl">Election is over</span>
-
-                </template>
-            </template>
-
-        </template>
-        <template v-else-if="state.Voted">
-            <span class="bg-green-200 text-green-600 py-2 px-5 rounded-full text-2xl">Voted</span>
-            <template v-if="isPastDay(state.election?.election_date)">
-                <span class="bg-red-200 text-red-600 py-2 px-5 rounded-full text-2xl">Election is over</span>
-            </template>
-        </template>
-
-
+    <div class="m-2 grid grid-cols-7 gap-4">
+        <div class="col-start-1 col-end-5">
+            <candidateListView
+                :candidates="state.candidates"
+                v-on:candidate="state.candidate_id = $event"
+            />
+            <div
+                class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                <span class="text-xs xs:text-sm text-gray-900">
+                    Showing {{ pagination.current_page }} of {{ pagination.last_page }} Entries
+                </span>
+                <div class="inline-flex mt-2 xs:mt-0">
+                    <button
+                        class="bg-yellow-500 hover:bg-yellow-400 border-yellow-700 hover:border-yellow-500 text-white font-bold py-1 px-4 ml-3 border-b-4 rounded"
+                        :disabled='!pagination.prev_page_url'
+                        :class="{'opacity-50': !pagination.prev_page_url }"
+                        @click="getCandidates(pagination.prev_page_url)"
+                    >
+                        Prev
+                    </button>
+                    <button
+                        class="bg-yellow-500 hover:bg-yellow-400 border-yellow-700 hover:border-yellow-500 text-white font-bold py-1 px-4 ml-3 border-b-4 rounded"
+                        :disabled='!pagination.next_page_url'
+                        :class="{'opacity-50': !pagination.next_page_url }"
+                        @click="getCandidates(pagination.next_page_url)"
+                    >
+                        Next
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-start-5 col-end-7">
+            <pie-chart v-if="!isSameDay(state.election?.election_date) && isPastDay(state.election?.election_date)"
+               :data="state.data"></pie-chart>
+        </div>
     </div>
+
+
+
+
+
+
+
     <Modal v-if="isModalOpen" @close-modal="isModalOpen = false" >
         <template #title>
             Enter password
