5aa7efd63648560baed6b4465ff78fd3c3b554f4
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-04-28 00:31:43 +0300

    Updated candidate voting page

--- resources/js/vue/candidateListView.vue
+++ resources/js/vue/candidateListView.vue
@@ -1,27 +1,8 @@
 <template>
-    <div class="container mx-auto px-4 sm:px-8">
-        <div class="py-8">
-            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-                <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                    <table class="min-w-full leading-normal">
-                        <thead>
-                        <tr>
-                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">#</th>
-                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">Firstname</th>
-                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">Lastname</th>
-                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">Birthdate</th>
-                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">Vote</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                                <ListCandidate v-for="(candidate, index) in candidates" :key="index"
-                                    :candidate="candidate" :index="index" v-on:selected-candidate="$emit('candidate', $event)"
-                                />
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+    <div class="container mx-auto px-4 sm:px-8 ">
+        <ListCandidate class="my-2 p-2" v-for="(candidate, index) in candidates" :key="index"
+            :candidate="candidate" :index="index" v-on:selected-candidate="$emit('candidate', $event)"
+        />
     </div>
 </template>
 
--- resources/js/vue/listCandidate.vue
+++ resources/js/vue/listCandidate.vue
@@ -1,23 +1,20 @@
 <template>
-    <tr>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
-            <p class="text-gray-900 whitespace-no-wrap text-center">foto veliau</p>
-        </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
-            <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.firstname }}</p>
-        </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
-            <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.lastname }}</p>
-        </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
-            <p class="text-gray-900 whitespace-no-wrap text-center">{{ candidate.birthdate }}</p>
-        </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-4xl">
-            <label class="flex justify-center ml-6">
-                <input v-model="state.candidate_id" v-on:click="$emit('selected-candidate', candidate.id)" type="radio" class="form-radio w-8 h-8" name="accountType">
-            </label>
-        </td>
-    </tr>
+    <div class="rounded overflow-hidden shadow-lg border border-gray-300">
+
+        <div class="grid grid-cols-7">
+            <div class="col-start-1 col-end-1">
+                <label class="flex justify-center ml-6">
+                    <input v-model="state.candidate_id" v-on:click="$emit('selected-candidate', candidate.id)" type="radio" class="form-radio w-8 h-8" name="accountType">
+                </label>
+            </div>
+            <div class="col-start-2 col-end-3">
+                <div class="font-semibold text-4xl mb-2">{{ candidate.firstname }}</div>
+            </div>
+            <div class="col-start-3 col-end-4">
+                <div class="font-semibold text-4xl mb-2">{{ candidate.lastname }}</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
