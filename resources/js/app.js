5e52664656c786e552e1462d14295a0aad20af1d
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-05-09 22:46:54 +0300

    init translations to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,25 +3,41 @@ require('./bootstrap');
 
 
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import store from './store';
 import Master from "./layouts/Master";
 import router from './routes';
+import i18n from './i18n';
 require('./store/subscriber')
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {faPersonBooth, faPlusSquare, faTrash, faCheck, faUser, faSignOutAlt, faSignInAlt, faSpinner, faHome, faUsers, faUsersCog, faVoteYea} from '@fortawesome/free-solid-svg-icons'
+import {faPersonBooth, faPlusSquare, faTrash, faCheck, faUser, faSignOutAlt, faSignInAlt, faSpinner, faHome, faUsers, faUsersCog, faVoteYea, faChevronDown} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
 
 
-library.add(faPlusSquare, faTrash, faPersonBooth, faCheck, faUser, faSignOutAlt, faSignInAlt, faSpinner, faHome, faUsers, faUsersCog, faVoteYea)
+library.add(faPlusSquare, faTrash, faPersonBooth, faCheck, faUser, faSignOutAlt, faSignInAlt, faSpinner, faHome, faUsers, faUsersCog, faVoteYea, faChevronDown)
+
+router.beforeEach( async (to, from , next) => {
+
+    let language = to.params.lang;
+
+    if(!language) {
+        language = 'en'
+    }
+
+    i18n.global.locale = language
+    next();
+})
+
+
 
 store.dispatch('attempt', localStorage.getItem('access_token')).then(() => {
     createApp({
         components: {
             Master
         }
-    }).use(router).use(store).use(VueChartkick).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+    }).use(router).use(i18n).use(store).use(VueChartkick).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
 })
