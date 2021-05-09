ff7bd62a7f2211d3fcb79fe6f1b3e335bd5dc8e9
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-05-09 22:48:21 +0300

    Update routed to have language in it

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,70 +1,84 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import store from './store';
+import { h, resolveComponent } from 'vue'
+import i18n from './i18n';
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: () => import('./views/Home'),
-        meta: {
-            requiresAuth: true
-        }
+        redirect: `/${i18n.loacle}/elections`
     },
     {
-        path: '/elections',
-        name: "Elections",
-        component: () => import('./views/Elections'),
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/elections/:electionId/candidates',
-        name: "Candidates",
-        component: () => import('./views/Candidates'),
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: () => import('./views/Login'),
-        meta: {
-            requiresVisitor: true
-        }
-    },
-    {
-        path: '/logout',
-        name: 'Logout',
-        component: () => import('./views/Logout'),
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/admin/elections',
-        name: 'AdminElections',
-        component: () => import('./views/AdminElections'),
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/admin/candidates',
-        name: "AdminCandidates",
-        component: () => import('./views/AdminCandidates'),
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/:chatchAll(.*)',
-        name: 'PageNotFound',
-        component: () => import('./views/PageNotFound'),
-        meta: {
-            requiresVisitor: true
-        }
+        path: '/:lang',
+        component: {
+            render() { return h(resolveComponent('router-view'))}
+        },
+        children: [
+            {
+                path: '/',
+                name: 'Home',
+                component: () => import('./views/Home'),
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: 'elections',
+                name: "Elections",
+                component: () => import('./views/Elections'),
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: 'elections/:electionId/candidates',
+                name: "Candidates",
+                component: () => import('./views/Candidates'),
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: 'login',
+                name: 'Login',
+                component: () => import('./views/Login'),
+                meta: {
+                    requiresVisitor: true
+                }
+            },
+            {
+                path: 'logout',
+                name: 'Logout',
+                component: () => import('./views/Logout'),
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: 'admin/elections',
+                name: 'AdminElections',
+                component: () => import('./views/AdminElections'),
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: 'admin/candidates',
+                name: "AdminCandidates",
+                component: () => import('./views/AdminCandidates'),
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: ':chatchAll(.*)',
+                name: 'PageNotFound',
+                component: () => import('./views/PageNotFound'),
+                meta: {
+                    requiresVisitor: true
+                }
+            }
+        ]
     }
 ]
 
@@ -74,6 +88,8 @@ const router = createRouter({
 });
 
 router.beforeEach( async (to, from , next) => {
+
+
 
 
     if(to.matched.some(record => record.meta.requiresAuth)) {
