442923454580465ccec7699501e5a24d5fc775a1
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-05-09 22:47:57 +0300

    Add en and lt locales

---
+++ resources/js/i18n.js
@@ -0,0 +1,28 @@
+import { createI18n } from 'vue-i18n';
+
+
+function loadLocaleMessages () {
+    const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+    const messages = {}
+    locales.keys().forEach(key => {
+      const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+      if (matched && matched.length > 1) {
+        const locale = matched[1]
+        messages[locale] = locales(key)
+      }
+    })
+    return messages
+  }
+
+
+
+const i18n = createI18n({
+    globalInjection: true,
+    legacy: false,
+    locale: 'en',
+    messages: loadLocaleMessages()
+  })
+
+
+export default i18n
+
