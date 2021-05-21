e43e2e72a70b270e33a18bf9fa20023a4ded3036
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-05-21 16:31:51 +0300

    Login page LT language update

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -6,8 +6,8 @@
 
         <div class="gb-white max-w-lg mx-auto p-8 md:p-8 my-5">
             <section>
-                <h3 class="font-bold text-2xl">Election System</h3>
-                <p class="text-gray-700 pt-2">Log In to your account</p>
+                <h3 class="font-bold text-2xl">{{ t("header.name") }}</h3>
+                <p class="text-gray-700 pt-2">{{ t("login.login1") }}</p>
 
             </section>
 
@@ -18,19 +18,19 @@
 
                     <div v-if="state.errors && state.errors.username " class="mb-2 text-sm py-2 px-3 bg-pink-200 text-red-700 rounded">{{ state.errors.username[0] }}</div>
                     <div class="mb-6 pt-3 rounded bg-gray-200">
-                        <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="username">Firstname</label>
+                        <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="username">{{ t("login.firstname") }}</label>
                         <input type="text" name="username"  id="username" class="login-input" v-model="state.username">
                     </div>
                     <div v-if="state.errors && state.errors.password " class="mb-2 text-sm py-2 px-3 bg-pink-200 text-red-700 rounded">{{ state.errors.password[0] }}</div>
                     <div class="mb-6 pt-3 rounded bg-gray-200">
-                        <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Password</label>
+                        <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">{{ t("login.password") }}</label>
                         <input type="password" name="password" id="password" class="login-input" v-model="state.password">
                     </div>
                     <div class="flex justify-end">
-                        <a href="#" class=" text-sm text-purple-600 hover:underline mb-6">Forgot your password?</a>
+                        <a href="#" class=" text-sm text-purple-600 hover:underline mb-6">{{ t("login.forgot") }}</a>
                     </div>
                     <button type="submit" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200">
-                        Log In
+                        {{ t("login.login2") }}
                         <font-awesome-icon v-if="state.loading" class="animate-spin" icon="spinner" />
                     </button>
                 </form>
@@ -46,6 +46,7 @@ import {reactive} from "vue";
 import { useStore } from 'vuex';
 import {useRouter} from "vue-router";
 import { Notification } from '../composables/Notify.js';
+import { useI18n } from 'vue-i18n'
 
 export default {
 
@@ -55,6 +56,8 @@ export default {
         const store = useStore();
 
         let { SendNotification } = Notification();
+
+        const { t } = useI18n()
 
         const state = reactive({
             username: '',
@@ -90,7 +93,8 @@ export default {
 
         return {
             state,
-            login
+            login,
+            t
         }
     }
 }
