6d62236e3799a179bb58f83188b1442d8764c821
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-05-21 10:46:25 +0300

    deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         <title>Election System</title>
 
         <!-- Fonts -->
